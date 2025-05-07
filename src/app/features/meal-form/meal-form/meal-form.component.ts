8b1769c542ff33314a9efa42cefad4f3ad04739e
Allison Shapanka Ash <allison.shapanka@gmail.com>
2025-05-07 11:09:02 -0400

    meal form: created form, added default values

--- src/app/features/meal-form/meal-form/meal-form.component.ts
+++ src/app/features/meal-form/meal-form/meal-form.component.ts
@@ -1,16 +1,37 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-meal-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './meal-form.component.html',
-  styleUrl: './meal-form.component.scss'
+  styleUrl: './meal-form.component.scss',
 })
 export class MealFormComponent {
-  constructor(private router: Router) {}
-  
+  mealForm: FormGroup;
+
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+  ) {
+    this.mealForm = this.fb.group({
+      timeAvailable: [''],
+      ingredients: [''],
+      dietaryRestrictions: this.fb.group({
+        glutenFree: [false],
+        dairyFree: [false],
+        vegetarian: [false],
+        peanutAllergy: [false],
+        other: [false],
+      }),
+      otherRestriction: [''],
+      pickyEaters: [false],
+    });
+  }
+
   onSubmit() {
     // Process form data here
     // Then navigate to results
